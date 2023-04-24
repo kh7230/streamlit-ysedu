55fdfd70b69757fb6b130e51dca6e4687dc969f4
kh7230 <wnrkdgml7730@gmail.com>
2023-04-24 10:28:47 +0900

    updated

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 # -*- coding:UTF-8 -*-
 import streamlit as st
 
-1 usage
+
 def main():
+    # title
     st.title("Hello World")
 
-if __name__ == '__main__':
+    # text
+    st.text('This is so {}'.format("good"))
+
+    # Header
+    st.header("This is Header")
+
+    # Subheader
+    st.subheader('This is subHeader')
+
+    # Markdown
+    st.markdown('## This is Markdown')
+
+    # 색상이 들어간 텍스트 feature
+    st.success('성공')
+    st.warning('경고')
+    st.info('정보와 관련된 탭')
+    st.error('에러 메시지')
+    st.exception('예외처리')
+
+    # st.write()
+    st.write('일반텍스트')
+    st.write(1 + 2)
+    st.write(dir(str))
+
+    st.title(':sunglasses:')
+
+    # Help
+    st.help(range)
+    st.help(st.title)
+
+    # 데이터 불러오기
+
+
+if __name__ == "__main__":
     main()
